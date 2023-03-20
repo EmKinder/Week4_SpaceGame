92dfa1254af825d70d569cbb51ea5c75989f27cd
EmKinder <89366821+EmKinder@users.noreply.github.com>
2023-03-20 13:04:57 +1100

    Implemented US23 + US33
    - US23:As a enemy I want to be able to hit the player a number of times so that I have room for mistake [15 min]
    - US33:As a player I want 3 lives, with 1 going down every time I am hit [20 mins]

--- Week4_SpaceGame/Assets/Em-Scripts/EnemyLaser.cs
+++ Week4_SpaceGame/Assets/Em-Scripts/EnemyLaser.cs
@@ -8,23 +8,38 @@ public class EnemyLaser : MonoBehaviour
     public GameObject player;
     private Rigidbody rb;
     public float force;
+    PlayerHealth ph;
+    float timer; 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player");
+        ph = player.GetComponent<PlayerHealth>();
 
         Vector3 direction = player.transform.position - transform.position;
         rb.velocity = new Vector3(direction.x, direction.y, direction.z).normalized * force;
         transform.LookAt(player.transform);
         transform.Rotate(90.0f, 0.0f, 0.0f);
-        // float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(rot, 0, 0);
+
     }   
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime; 
+        if(timer > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ph.BeenHit(1);
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Week4_SpaceGame/Assets/Em-Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int startingHealth = 3;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeenHit(int livesLost)
+    {
+        currentHealth -= livesLost;
+        if(currentHealth == 0)
+        {
+            //Game Over
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            //Update UI
+            Debug.Log("Hit! Lives Remaining:" + currentHealth);
+        }
+    }
+}
