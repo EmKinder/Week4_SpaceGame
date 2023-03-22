5d20d15df7654886fc7c3cc24d20a1829faf5500
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-03-22 14:58:52 +1100

    Implemented US35
    U35: As a player, I want to see my score increase as I progress through the game so I can see my progress

--- Week4_SpaceGame/Assets/Em-Scripts/EnemyDeath.cs
+++ Week4_SpaceGame/Assets/Em-Scripts/EnemyDeath.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class EnemyDeath : MonoBehaviour
 {
     Animator anim;
+    PlayerScore ps;
+    public bool enemyDeathTest;
+    public int enemiesLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();   
+        anim = GetComponent<Animator>();
+        ps = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScore>();
+        enemyDeathTest = false;
+        enemiesLeft = 27;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemyDeathTest)
+        {
+            ThisEnemyDeath();
+            enemyDeathTest = false;
+        }
     }
 
     public void ThisEnemyDeath()
     {
         Debug.Log("Script Triggered");
-        StartCoroutine(ThisEnemyDeathIEnumerator(5.0f));
+        ps.SetScore(100);
+        StartCoroutine(ThisEnemyDeathIEnumerator(3.0f));
     }
 
     private IEnumerator ThisEnemyDeathIEnumerator(float waitTime)
@@ -31,7 +42,18 @@ public class EnemyDeath : MonoBehaviour
             Debug.Log("Coroutine Called");
             anim.SetTrigger("wobble");
             yield return new WaitForSeconds(waitTime);
+            SetEnemiesLeftInScene();
+            Destroy(this.gameObject.transform.parent.gameObject);
             Destroy(this.gameObject);
+        }
+    }
+    
+    void SetEnemiesLeftInScene()
+    {
+        enemiesLeft -= 1;
+        if(enemiesLeft == 0)
+        {
+            Debug.Log("You Win!");
         }
     }
 }
--- Week4_SpaceGame/Assets/Em-Scripts/PlayerScore.cs
+++ Week4_SpaceGame/Assets/Em-Scripts/PlayerScore.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
+    public int score;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        scoreText.text = score.ToString("000000");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = score.ToString("000000");
     }
+
+    public void SetScore(int thisScore)
+    {
+        score += thisScore;
+    }
+    
 }
