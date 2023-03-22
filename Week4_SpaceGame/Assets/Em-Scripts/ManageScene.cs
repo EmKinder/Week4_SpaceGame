6633c819340fab60b589123de135ee9d2d309281
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-03-22 15:55:45 +1100

    Implemented US38
    US38: As a player, I'd like a pause button so I can stop the game whenever I'd like

--- Week4_SpaceGame/Assets/Em-Scripts/ManageScene.cs
+++ Week4_SpaceGame/Assets/Em-Scripts/ManageScene.cs
@@ -2,19 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
 
 public class ManageScene : MonoBehaviour
 {
+    bool gamePaused;
+    public AudioSource backgroundMusic;
+    public Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gamePaused = false;
+        canvas.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PauseGame(bool ifGamePaused)
+    {
+        if (!gamePaused)
+        {
+            backgroundMusic.Pause();
+            Time.timeScale = 0.0f;
+            gamePaused = true;
+            canvas.enabled = true;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            backgroundMusic.Play();
+            gamePaused = false;
+            canvas.enabled = false;
+
+        }
     }
 
     public void RestartLevel()
