032db12fab2f49e4dcf3feda2391271861a6503e
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-03-22 15:27:23 +1100

    Implemented U18
    US18: As a player, I want a health bar to see how much damage I have taken

--- Week4_SpaceGame/Assets/Em-Scripts/PlayerHealth.cs
+++ Week4_SpaceGame/Assets/Em-Scripts/PlayerHealth.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     float startingHealth = 3;
     public float currentHealth;
+    public Image leftHeart1;
+    public Image rightHeart1;
+    public Image leftHeart2;
+    public Image rightHeart2;
+    public Image leftHeart3;
+    public Image rightHeart3;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +23,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ViewableLives();
     }
 
     public void BeenHit(float livesLost)
@@ -30,6 +38,79 @@ public class PlayerHealth : MonoBehaviour
         {
             //Update UI
             Debug.Log("Hit! Lives Remaining:" + currentHealth);
+        }
+    }
+
+    void ViewableLives()
+    {
+        if (currentHealth == 3)
+        {
+            rightHeart3.enabled = true;
+            leftHeart3.enabled = true;
+            rightHeart2.enabled = true;
+            leftHeart2.enabled = true;
+            rightHeart1.enabled = true;
+            leftHeart1.enabled = true;
+        }
+
+        if (currentHealth == 2.5)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = true;
+            rightHeart2.enabled = true;
+            leftHeart2.enabled = true;
+            rightHeart1.enabled = true;
+            leftHeart1.enabled = true;
+        }
+        if (currentHealth == 2)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = false;
+            rightHeart2.enabled = true;
+            leftHeart2.enabled = true;
+            rightHeart1.enabled = true;
+            leftHeart1.enabled = true;
+
+        }
+        if (currentHealth == 1.5)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = false;
+            rightHeart2.enabled = false;
+            leftHeart2.enabled = true;
+            rightHeart1.enabled = true;
+            leftHeart1.enabled = true;
+
+        }
+        if (currentHealth == 1)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = false;
+            rightHeart2.enabled = false;
+            leftHeart2.enabled = false;
+            rightHeart1.enabled = true;
+            leftHeart1.enabled = true;
+
+        }
+        if (currentHealth == 0.5)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = false;
+            rightHeart2.enabled = false;
+            leftHeart2.enabled = false;
+            rightHeart1.enabled = false;
+            leftHeart1.enabled = true;
+
+        }
+        if (currentHealth <= 0)
+        {
+            rightHeart3.enabled = false;
+            leftHeart3.enabled = false;
+            rightHeart2.enabled = false;
+            leftHeart2.enabled = false;
+            rightHeart1.enabled = false;
+            leftHeart1.enabled = false;
+
         }
     }
 }
